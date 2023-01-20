9c6acb19c739658a733bce595f7eb561a9b0df1f
vishal kaira <103746076+Vishal-sundar-kaira@users.noreply.github.com>
2023-01-20 16:17:13 +0530

    Add files via upload

---
+++ food_data_csi.py
@@ -0,0 +1,145 @@
+
+# In[303]:
+
+
+import pandas as pd
+import numpy as np
+import sys
+
+# In[304]:
+df1=pd.read_csv('food_data.csv', encoding= 'unicode_escape')
+
+
+# In[305]:
+
+
+# df1.head()
+
+
+# In[306]:
+
+
+import datetime
+
+
+# In[307]:
+
+
+today = datetime.date.today()
+
+
+# In[308]:
+
+
+print(today)
+
+
+# In[309]:
+
+
+ts=pd.Timestamp(today)
+
+
+# In[310]:
+
+
+# ts
+
+
+# In[311]:
+
+
+df1['use by'] = pd.to_datetime(df1['use by'], errors='coerce')
+
+
+# In[312]:
+
+
+# df1.info()
+
+
+# In[313]:
+
+
+ind=df1['use by'].index
+
+
+# In[314]:
+
+
+# ind
+
+
+# In[315]:
+
+
+#df1['use by'] = df1['use by'].dt.strftime('%d/%m/%Y')
+
+
+# In[316]:
+
+
+# df1.info()
+
+
+# In[317]:
+
+
+# df1.head()
+
+
+# In[318]:
+
+
+l=[]
+for x in df1['use by']:
+    l.append((x-ts))
+
+
+# In[319]:
+
+
+# l
+
+
+# In[320]:
+
+
+z=pd.Series(l)
+
+
+# In[321]:
+
+
+# z
+
+
+# In[322]:
+
+
+df1['diff']=z
+
+
+# In[323]:
+
+
+# df1.head()
+
+
+# In[324]:
+
+
+df1=df1.sort_values(by='diff',ascending=True)
+
+
+# In[325]:
+
+for i in df1['diff']:
+    print(i)
+
+
+# In[ ]:
+
+
+
+
---
+++ ngos_csi.py
@@ -0,0 +1,182 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+# 19.125076600000014, 72.83273467301058
+
+
+# **Current coordinates**
+
+# In[2]:
+
+
+import requests
+
+
+# In[3]:
+
+
+res=requests.get('https://ipinfo.io')
+
+
+# In[4]:
+
+
+# print(res.text)
+
+
+# In[5]:
+
+
+dt=res.json()
+
+
+# In[6]:
+
+
+# print(dt)
+
+
+# In[7]:
+
+
+# print(dt['loc'])
+
+
+# In[8]:
+
+
+loc=dt['loc'].split(',')
+
+
+# In[9]:
+
+
+lat=loc[0]
+long=loc[1]
+
+
+# In[10]:
+
+
+# type(lat)
+
+
+# In[11]:
+
+
+lat=float(lat)
+
+
+# In[12]:
+
+
+# type(lat)
+
+
+# In[13]:
+
+
+long=float(long)
+
+
+# In[14]:
+
+
+import pandas as pd
+import numpy as np
+
+
+# In[15]:
+
+
+df=pd.read_csv('ngo.csv')
+
+
+# In[16]:
+
+
+# df.head()
+
+
+# In[17]:
+
+
+# df.info()
+
+
+# In[18]:
+
+
+diff_latitude=[]
+for x in df['latitude'] :
+    diff_latitude.append(abs(x-lat))
+
+
+# In[19]:
+
+
+diff_longitude=[]
+for x in df['longitude'] :
+    diff_longitude.append(abs(x-long))
+
+
+# In[20]:
+
+
+latitude_difference=pd.Series(diff_latitude)
+longitude_difference=pd.Series(diff_longitude)
+
+
+# In[21]:
+
+
+df1=df.copy()
+
+
+# In[22]:
+
+
+# df1.head()
+
+
+# In[23]:
+
+
+df1=pd.concat([df1,latitude_difference,longitude_difference],axis=1)
+
+
+# In[24]:
+
+
+df1.columns=['ngo','address','latitude','longitude','latitudediff','longitudediff']
+
+
+# In[25]:
+
+
+# df1.head()
+
+
+# In[26]:
+
+
+df1=df1.sort_values(by=['latitudediff','longitudediff'],ascending=True)
+
+
+# In[27]:
+n=[]
+for i in df1['address']:
+    n.append(i)
+print(n)
+# for x in range(len(df1)):
+#     print(df1.loc[x,'ngo'],'\t',df1.loc[x,'address'])
+
+
+# In[ ]:
+
+
+
+
